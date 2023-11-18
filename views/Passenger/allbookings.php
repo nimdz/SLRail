cdd11dd8f0764c3d68a8bef5bc97eec59e52536f
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-11-18 18:59:19 +0530

    Update views of passenger

--- views/Passenger/allbookings.php
+++ views/Passenger/allbookings.php
@@ -4,74 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>My Bookings</title>
-    <style>
-        table, td, th {  
-            border: 1px solid #ddd;
-            text-align: left;
-        }
-
-        table {
-            border-collapse: collapse;
-            width: 100%;
-        }
-
-        th, td {
-            padding: 15px;
-        }
-
-        p {
-            font-size: 32px;
-        }
-
-        h1 {
-            font-size: 42px;
-        }
-
-        button {
-            width: 100%;
-            padding: 10px;
-            border: none;
-            border-radius: 50px;
-            background-color: #007bff;
-            color: #fff;
-            font-size: 16px;
-            font-weight: bold;
-            cursor: pointer;
-            transition: background-color 0.3s ease;
-        }
-
-        button + button {
-            margin-top: 10px;
-        }
-
-.subfooter {
-    margin-left: 0px;
-    position: fixed;
-    bottom: 0;
-    width: 100%;
-    height: 40px;
-    background-color: #f9f9f9bb;
-}
-
-.subfooter-container {
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-    align-items: center;
-    background-color: #f9f9f9bb;
-
-}
-    </style>
+    <link rel="stylesheet" href="/SlRail/public/css/table.css">
 </head>
 <body>
    
-    <div class="container" style="background-color: brown; text-align: center; margin-left:opx; ">
-        <p style="color: white; text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);">
-            <span style="font-weight: bold;">SL Rail - </span>
-            <span style="font-style: normal;">Revolutionizing Sri Lankan Rail Travel</span>
-        </p>
-    </div>
-    <h1 style="margin-top:100px"><center>Your Bookings</center></h1>
+   <?php include('includes/header.php'); ?>
+
+    <h1><center>Your Bookings</center></h1>
     <table>
         <thead>
             <tr>
@@ -136,10 +75,7 @@
     }
 }
     </script>
-      <div class="subfooter">
-        <div class="subfooter-container">
-            <h5>&copy; 2023 SL Rail. All rights reserved.</h5>
-        </div>
-    </div>
+        <?php include('includes/footer.php'); ?>
+
 </body>
 </html>
