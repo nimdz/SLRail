39ba4ea6f7577e94e24b6f676a82991b6c5e9fb4
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-11-30 19:23:35 +0530

    Update AnnouncementController.php

--- controllers/AnnouncementController.php
+++ controllers/AnnouncementController.php
@@ -8,6 +8,31 @@ class AnnouncementController{
 
         session_start();
 
+        include 'views/StationMaster/announcement_add.php';
+
+
+        $title=$_POST["title"];
+        $description=$_POST["description"];
+
+        $annModel=new AnnouncementModel();
+        $annResult=$annModel->addAnnouncement($title,$description);
+
+        if($annResult){
+            echo '<script>alert("Announcement Added Successfully!"); window.location.href = "/SlRail/stationmaster/dashboard";</script>';
+        }else{
+            echo '<script>alert("Error: Review Addition Failed");window.location.href=",/SlRail/stationmaster/dashboard";</script>';
+
+        }
+    
+    
+    }
+    public function addAnnouncementD(){
+
+        session_start();
+
+        include 'views/TrainDriver/announcement_add.php';
+
+
         $title=$_POST["title"];
         $description=$_POST["description"];
 
