9494faef1626e02084f7849653f20515c07feed1
Binuri Mindula <binuriwagarachchi@gmail.com>
2023-12-03 21:41:48 +0530

    update dashboard

--- views/StationMaster/sm_dashboard.php
+++ views/StationMaster/sm_dashboard.php
@@ -1,3 +1,8 @@
+<?php
+// Set the active link based on the current page
+$activeLink = 'dashboard'; // Change this value according to the current page
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,7 +16,7 @@
 </head>
 <body>
     
-    <div class="sidebar">
+    <!--<div class="sidebar">
 
       <a href="#"><img width="100px" src="/SlRail/public/assets/logo.jpg"> </a>
 
@@ -30,9 +35,10 @@
                   calendar_month
                 </span>
                 Add Train Schedule
-            </a>
+            </a>-->
 
-            <a href="/SlRail/announcement/addAnnouncement">
+            <!--<a href="/SlRail/announcement/addAnnouncement">-->
+          <!--  <a href="/SlRail/views/StationMaster/sm_add_announcements.php">
                  <span class="material-symbols-outlined">
                   campaign
                 </span>
@@ -59,24 +65,25 @@
            <a>
              
       
-    </div>
-
+    </div>-->
+    
+    <?php include('sm_sidebar.php'); ?>
     <?php include('includes/header.php'); ?>
 
        
 
     <div class="content">
       <h1> <center>Welcome StationMaster! </center></Welcome></h1>
-      <p>"Welcome to the Station Master Dashboard, your central hub for efficient railway management.
+      <p><center>"Welcome to the Station Master Dashboard, your central hub for efficient railway management.
          As a Station Master, you play a vital role in ensuring the smooth operation of our SL railway system. 
          From this dashboard, you have the power to manage your profile, view passenger details, and stay informed about feedback and inquiries. 
          You can also update train arrivals and departures, monitor ticket refunds, add train schedules, and track the live location of trains in real-time. 
          This comprehensive set of tools empowers you to provide top-notch service to passengers and maintain the integrity of our railway network. 
          Feel free to navigate through the menu on the left to access these functionalities and streamline your daily tasks. 
          We're committed to making your role as a Station Master as efficient and effective as possible, contributing to a seamless railway experience for all passengers." 
-     </p>
+         </center></p>
    </div>
-
+  
   <?php include('includes/footer.php'); ?>
     
 </body>
