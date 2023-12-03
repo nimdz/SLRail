18426b4061328aea2a39262503c2fff43360c41a
Binuri Mindula <binuriwagarachchi@gmail.com>
2023-12-03 21:36:42 +0530

    Update sm_add_announcements file

--- views/StationMaster/sm_add_announcements.php
+++ views/StationMaster/sm_add_announcements.php
@@ -3,6 +3,12 @@
 $activeLink = 'a_add'; // Change this value according to the current page
 ?>
 
+<?php
+// Set default values for $data
+$data = array('title' => '', 'body' => '', 'title_err' => '', 'body_err' => '');
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,8 +16,7 @@ $activeLink = 'a_add'; // Change this value according to the current page
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SL Rail</title>
-    <link rel="stylesheet" href="/SlRail/public/css/styles.css">
-    <link rel="stylesheet" href="/SlRail/public/css/media-queries.css">
+    
     <link rel="stylesheet" href="/SlRail/public/css/form.css">
 
     <!-- Cancel & Update btns -->
@@ -32,48 +37,37 @@ $activeLink = 'a_add'; // Change this value according to the current page
 
     <?php include('sm_sidebar.php'); ?>
     
-    <?php include('includes/header.php'); ?>
+    <?php include('/includes/header.php'); ?>
     
-    <!-- Page content -->
-<div class="content">
-    <section class="features-section">
+    <div class="content">
+    <section class="announcement-section">
         <div class="container">    
-            <h3>Add New Announcement</h3>
+        <h3>Add New Announcement</h3>
         </div>
         <div class="container">
-            <div class="container">
-                <form action="action_page.php" method="POST">
-                  <div class="row">
-                    <div class="col-25">
-                      <label for="titl">Title</label>
-                    </div>
-                    <div class="col-75">
-                      <input type="text" id="titl" name="title" value="<?php echo $data['title']; ?>">
-                      <span class="form-invalid"><?php echo $data['title_err']; ?></span>
-
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div class="col-25">
-                      <label for="desc">Description</label>
-                    </div>
-                    <div class="col-75">
-                      <textarea id="desc" name="subject" placeholder="Write something.." style="height:200px"><?php echo $data['body']; ?></textarea>
-                      <span class="form-invalid"><?php echo $data['body_err']; ?></span>
-                      
-                    </div>
-                  </div>
-                  <div class="row">
-                    <div>
-                        <input type="submit" value="Add Passenger" class="update-btn", id="updatePro">
-                        <button type="button" id="cancelButton" class="cancel-btn">Cancel</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
+        
+        <form action="/SlRail/controllers/StationMasterController/createAnnouncement" method="POST" class="announcement-form">
+        <div class="row">
+            <label for="title">Title</label>
+            <input type="text" id="title" name="title" value="<?php echo $data['title']; ?>">
+            <span class="form-error"><?php echo $data['title_err']; ?></span>
+        </div>
+        <div class="row">
+        <label for="description">Description</label>
+        <textarea id="description" name="description" placeholder="Write something.." style="height:200px"><?php echo $data['body']; ?></textarea>
+        <span class="form-error"><?php echo $data['body_err']; ?></span>
+    </div>
+    <div class="row">
+        <div class="button-group">
+            <input type="submit" value="Add Announcement" class="update-btn" id="updateBtn">
+            <button type="button" id="cancelBtn" class="cancel-btn">Cancel</button>
+        </div>
+    </div>
+</form>
         </div>        
     </section>
 </div>
+
 
     <!-- subfooter -->
 <?php include('includes/footer.php'); ?>
