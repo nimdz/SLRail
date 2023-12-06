26f85f763c7b96ba3b22f688814564c9df8ee518
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-12-06 18:53:19 +0530

    Update TraindriverController.php

--- controllers/TraindriverController.php
+++ controllers/TraindriverController.php
@@ -4,31 +4,6 @@ require_once 'models/Employee/EmployeeModel.php';
 
 class TraindriverController{
 
-
-    public function login(){
-    
-     session_start();
-
-     include 'views/TrainDriver/td_login.php';
-
-     if($_SERVER['REQUEST_METHOD']==='POST'){
-          $username=$_POST['username'];
-          $password=$_POST['password'];
-
-          $tdModel=new EmployeeModel();
-          $user=$tdModel->loginEmployee($username, $password);
-
-          if($user){
-               if($user['position']==="2"){
-                   header("Location:/SlRail/traindriver/dashboard");
-              }else{
-                echo '<script>alert("Error: You do not have the required permission.")</script>';            
-             }
-           }else{
-          echo'<script>alert("Error:Login Failed.Check your credintals.")';
-        }
-      }
-    }
     public function profile()
     {
         // Start a session
@@ -98,7 +73,7 @@ public function updateProfile()
        $tdModel=new EmployeeModel();
        $tdModel->logoutEmployee();
 
-       header("Location:/SlRail/traindriver/login");
+       header("Location:/SlRail/home/login");
     }
 
   
