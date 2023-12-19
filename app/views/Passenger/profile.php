a7c67ec0ed16bf03d9d03a24c3cdf53c1cfc544a
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-12-19 22:10:04 +0530

    Update profile.php

--- app/views/Passenger/profile.php
+++ app/views/Passenger/profile.php
@@ -1,46 +1,90 @@
-<!-- passenger_profile.php -->
+<!-- driver_profile.php -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <title>Passenger Profile</title>
   <link rel="stylesheet" href="/SlRail/public/css/sidebar.css">
   <link rel="stylesheet" href="/SlRail/public/css/profile.css">
-  <script src="/SlRail/public/Js/profile.js"></script>
-  
+
+  <style>
+    .update-form input[type="text"] 
+    {
+      width: 200px;
+      padding: 10px;
+      margin-bottom: 20px;
+      border: none;
+      border-radius: 50px;
+      background-color: #fff;
+      box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+      margin-left: 400px;
+    }
+    .update-form{
+      margin-top: 10px;
+      border:2px solid black;
+      border-radius: 50px;
+      width:900px;
+      height:660px;
+      justify-content: center;
+    }
+    #profileDetails{
+      margin-top: 10px;
+      border:2px solid black;
+      border-radius: 50px;
+      width:900px;
+      height:660px;
+    }
+  </style>
 </head>
 <body>
 
+  <?php include('public/includes/header.php'); ?>
 
-    <?php include('public/includes/header.php'); ?>
+  <?php include('passenger_sidebar.php'); ?>
 
-    <?php include('passenger_sidebar.php'); ?>
+  <!-- Display profile details -->
+  <div id="profileDetails">
+    <h1>Profile</h1>
+    <img class="profile"src="/SlRail/public/assets/profile.jpg" alt="Profile Image">
+    <p class="info">Full Name: <?= $profile['full_name'] ?></p>
+    <p class="info">UserName: <?= $profile['username'] ?></p>
+    <p class="info">Email: <?= $profile['email'] ?></p>
+    <button class="profileButton" onclick="showUpdateForm()"style="margin-left:300px; border-radius:50px; width:200px; height:30px; background-color:blue; font-size:24px; ">Edit Profile</button>
+  </div>
 
+  <!-- Option to update profile -->
+  <div id="updateForm" class="update-form" style="margin-left:300px;">
+    <h2 style="margin-left:300px;">Update Profile</h2>
+    <form action="/SlRail/passenger/updateProfile" method="post">
+      <label for="full_name" style="margin-left:300px;">Full Name:</label>
+      <input type="text" id="full_name" name="full_name" value="<?= $profile['full_name'] ?>" required>
 
-        <!-- Display profile details -->
-        <div id="profileDetails">
-            <h1>Profile</h1>
-            <img class="profile"src="/SlRail/public/assets/profile.jpg" alt="Profile Image">
-            <p class="info">Full Name: <?= $profile['full_name'] ?></p>
-            <p class="info">Email: <?= $profile['email'] ?></p>
-            <button class="profileButton" onclick="showUpdateForm()">Edit Profile</button>
-        </div>
+      <label for="email" style="margin-left:300px;">Email:</label>
+      <input type="text"  id="email" name="email" value="<?= $profile['email'] ?>" required>
+                  
+      <label for="username" style="margin-left:300px;">Username:</label>
+      <input type="text"  id="username" name="username" value="<?= $profile['username'] ?>" required>
 
-        <!-- Option to update profile -->
-        <div id="updateForm" class="update-form" style=" margin-top:100px; width:700px; ">
-           <h2 >Update Profile</h2>
-            <form action="/SlRail/passenger/updateProfile" method="post">
-                <label for="full_name" style="margin-left:150px;">Full Name:</label>
-                <input type="text" id="full_name" name="full_name" value="<?= $profile['full_name'] ?>" style="border-radius:50px; margin-left:200px; width:200px;"required>
+      <button class="updateButton" type="submit" style="margin-left:400px; height:30px;">Save Changes</button>
+      <button class="cancelButton" type="button" onclick="hideUpdateForm()" style="margin-left:400px;">Cancel</button>
+    </form>
+  </div>
 
-                <label for="email" style="margin-left:150px; ">Email:</label>
-                <input type="email" id="email" name="email" value="<?= $profile['email']?>" style="border-radius:50px; margin-left:200px; width:200px;"required>
+  <?php include('public/includes/footer.php'); ?>
+  <script>
+    // Add this function to hide the update form when the page loads
+    document.addEventListener('DOMContentLoaded', function () {
+      hideUpdateForm();
+    });
 
-                <button class="updateButton" type="submit" style="border-radius:50px; margin-left:200px;  width:200px;">Save Changes</button>
-                <button class="cancelButton" type="button" onclick="hideUpdateForm()" style="border-radius:50px; margin-left:200px;  width:200px;">Cancel</button>
-            </form>
-        </div>
+    function showUpdateForm() {
+      document.getElementById('profileDetails').style.display = 'none';
+      document.getElementById('updateForm').style.display = 'block';
+    }
 
-    <?php include('public/includes/footer.php'); ?>
- 
+    function hideUpdateForm() {
+      document.getElementById('updateForm').style.display = 'none';
+      document.getElementById('profileDetails').style.display = 'block';
+    }
+  </script>
 </body>
 </html>
