fd57e0b60c75fc7f5778d731b9fbc0601285c6ab
Binuri Mindula <binuriwagarachchi@gmail.com>
2023-12-30 01:48:22 +0530

    Update Train Model

--- app/models/Employee/TrainModel.php
+++ app/models/Employee/TrainModel.php
@@ -31,6 +31,27 @@ class TrainModel
         }
     }
 
-    
+    public function getTrainByNumber($train_number)
+    {
+        $conn = $this->db->getConnection();
+
+        $sql = "SELECT train_number, train_type, capacity, stoppings FROM train WHERE train_number = ?";
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt === false) {
+            die("Error: " . $conn->error);
+        }
+
+        $stmt->bind_param("i", $train_number);
+        $stmt->execute();
+
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            return $result->fetch_assoc();
+        } else {
+            return null; // No matching train found
+        }
+    }
 }
 ?>
