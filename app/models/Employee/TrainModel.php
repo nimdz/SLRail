39d82ea6271274f22c2cc9156eaf750d786addac
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2023-12-31 11:26:06 +0530

    Create TrainModel.php

---
+++ app/models/Employee/TrainModel.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once 'app/includes/database.php';
+
+class TrainModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function createTrain($train_number, $train_type, $capacity, $stoppings)
+    {
+        $conn = $this->db->getConnection();
+
+        $sql = "INSERT INTO train (train_number, train_type, capacity, stoppings) VALUES (?, ?, ?, ?)";
+        $stmt = $conn->prepare($sql);
+
+        if ($stmt === false) {
+            die("Error: " . $conn->error);
+        }
+
+        $stmt->bind_param("isis", $train_number, $train_type, $capacity, $stoppings);
+
+        if ($stmt->execute()) {
+            return true; // Added successful
+        } else {
+            return false; // Added failed
+        }
+    }
+
+    
+}
+?>
