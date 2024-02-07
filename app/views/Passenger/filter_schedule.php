6123c5070672859f8d4b426f0e6ec96c3d114a77
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-02-07 21:50:27 +0530

    add some styles

--- app/views/Passenger/filter_schedule.php
+++ app/views/Passenger/filter_schedule.php
@@ -10,6 +10,55 @@
     <link rel="stylesheet" href="/SlRail/public/css/sidebar.css">
     <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <style>
+
+
+            form {
+ display: inline-block;
+  align-items: center;
+  margin: 20px auto;
+  font-family: sans-serif;
+  width: 1200px;
+  padding: 10px;
+}
+
+
+label {
+  display: inline-block;
+  margin-right: 20px;
+  font-weight: bold;
+}
+
+input[type="text"],
+input[type="submit"] {
+  display: inline-block;
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  border-radius: 50px;
+  margin-right: 30px;
+
+}
+
+input[type="text"] {
+  width: 150px;
+  border-radius: 50px;
+}
+
+input[type="submit"] {
+  background-color: #007bff;
+  color: white;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+}
+
+input[type="submit"]:hover {
+  background-color: #0062cc;
+}
+
+
+         
+          
+    </style>
 </head>
 
 <body class="clearfix">
@@ -17,17 +66,32 @@
     <?php include('public/includes/header.php'); ?>
 
     <?php include('passenger_sidebar.php'); ?>
+    <script src="/SlRail/public/Js/stations.js" type="text/javascript"></script>
+
 
     <h1 style="margin-left:270px; margin-top:10px;"><center>Train Schedule</center></h1>
 
-    <?php if (isset($availableTrains) && !empty($availableTrains)) : ?>
+    <form action="/SlRail/trainschedule/filter" method="get" style="text-align: center; margin-top: 15px; margin-left:50px;">
+       
+       <label for="startStation">Start Station:</label>
+       <select id="From" name="departure_station" required>
+       </select>
+
+       <label for="endStation">End Station:</label>
+       <select id="destination" name="destination_station" required>
+       </select>        
+
+       <input type="submit" value="Search">
+   </form>
+
+    <?php if (isset($availabletrains) && !empty($availabletrains)) : ?>
         <table class="trains-table" style="margin-top:5px; margin-left:220px;">
             <thead>
                 <tr>
                     <th><span class="material-symbols-outlined">train</span> Train ID</th>
                     <th><span class="material-symbols-outlined">train</span> Train Type</th>
-                    <th><span class="material-symbols-outlined">location_on</span> Departure Station</th>
-                    <th><span class="material-symbols-outlined">location_on</span> Destination Station</th>
+                    <th><span class="material-symbols-outlined">location_on</span> Departure </th>
+                    <th><span class="material-symbols-outlined">location_on</span> Destination </th>
                     <th><span class="material-symbols-outlined">schedule</span> Departure Time</th>
                     <th><span class="material-symbols-outlined">schedule</span> Arrival Time</th>
                     <th><span class="material-symbols-outlined">event</span> Availability</th>
