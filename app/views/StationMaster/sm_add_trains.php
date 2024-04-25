5ebfac4564c7862652a135fe4efed6076e0d76f9
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-04-26 04:45:10 +0530

    Update sm_add trains

--- app/views/StationMaster/sm_add_trains.php
+++ app/views/StationMaster/sm_add_trains.php
@@ -9,8 +9,8 @@ $activeLink = 'addTrains';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Add Trains</title>
-    <link rel="stylesheet" href="/SlRail/public/css/train_form.css">
-    <link rel="stylesheet" href="/SlRail/public/css/sidebar.css">
+    <link rel="stylesheet" href="/SlRail/public/css/StationMaster/train_form.css">
+    <link rel="stylesheet" href="/SlRail/public/css/StationMaster/sidebar.css">
     <style>
         .container{
             margin-top: 50px;
@@ -26,56 +26,49 @@ $activeLink = 'addTrains';
 
 <?php include('sm_sidebar.php'); ?>
 
-
 <div class="container">
-    <form action="/SlRail/train/addTrain" method="post">
+<form action="/SlRail/train/addTrain" method="post">
         <h3>Add Trains</h3>
+      <div class="form-row">
+        <label for="trainNumber">Train Number:</label>
+        <input type="int" id="trainNumber" name="trainNumber" required placeholder="">
+      </div>
 
-        <div class="row-trains">
-            <div class="col-25">
-                <label for="trainNum">Train Number:</label>
-            </div>
-            <div class="col-75">
-                <input type="int" id="trainNum" name="trainNum" required placeholder="">
-            </div>
+      <div class="form-row">
+        <label for="trainType">Train Type:</label>
+        <select id="train_type" name="train_type" required>
+                <option value="" disabled selected>Select Train Type</option>
+                    <option value="Express">Express</option>
+                    <option value="Intercity">Intercity</option>
+                    <option value="Slow">Slow</option>
+                </select>
+      </div>
 
-            <div class="col-25">
-                <label for="type">Train Type(Express/Intercity/Slow):</label>
-            </div>
-            <div class="col-75">
-                <input type="text" id="type" name="type" required placeholder="">
-            </div>
+      <div class="form-row">
+        <label for="trainModel">Train Model:</label>
+        <input type="text" id="trainModel" name="trainModel" required placeholder="">
+      </div>
+
+      <div class="form-row">
+        <label for="trainDriverId">Train Driver ID:</label>
+        <input type="int" id="trainDriverId" name="trainDriverId" required placeholder="">
+      </div>
+
+      <div class="form-row">
+        <label>Number of Seats:</label>
+        <div class="seat-inputs">
+          <input type="number" id="firstClassSeats" name="firstClassSeats" placeholder="1st Class" required>
+          <input type="number" id="secondClassSeats" name="secondClassSeats" placeholder="2nd Class" required>
+          <input type="number" id="thirdClassSeats" name="thirdClassSeats" placeholder="3rd Class" required>
         </div>
+      </div>
 
-        <div class="row-trains">
-            <div class="col-25">
-                <label for="capacity">Number of Passengers:</label>
-            </div>
-            <div class="col-75">
-                <input type="int" id="capacity" name="capacity" required placeholder="">
-            </div>
-
-            <div class="col-25">
-                <label for="stoppings">Stoppings:</label>
-            </div>
-            <div class="col-75">
-                <input type="text" id="stoppings" name="stoppings" required placeholder="">
-            </div>
-            
-        </div>
-
-        <div class="row">
-        <div class="col-50"> <!-- Adjusted column width for the buttons -->
-                <input type="submit" value="Send" class="update-btn" id="updatePro">
-            </div>
-            <div class="col-50">
-                <button type="button" value="Cancel" class="cancel-btn" id="cancel-btn"><a href="/SlRail/stationmaster/dashboard">Cancel</a></button>
-            </div>
-            
-        </div>
+      <div class="form-row">
+      <input type="submit" value="Send" class="update-btn" id="updatePro">
+      <button type="button" value="Cancel" class="cancel-btn" id="cancel-btn"><a href="/SlRail/stationmaster/dashboard">Cancel</a></button>
+      </div>
     </form>
-</div>
-
+  </div>
 
     <!-- footer -->
     <?php include('public/includes/footer.php'); ?>
