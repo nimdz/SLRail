947fb39beff060b0cea4ab1b182bb9b876420a02
Binuri Mindula <binuriwagarachchi@gmail.com>
2024-04-26 04:47:13 +0530

    update sm_track location

--- app/views/StationMaster/sm_track_location.php
+++ app/views/StationMaster/sm_track_location.php
@@ -8,50 +8,37 @@ $activeLink = 'track';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Track Location</title>
-    <link rel="stylesheet" type="text/css" href="/SlRail/public/css/styles_location.css">
-    
-    <style>
-      .btn1{
-        border-radius: 50px;
-        background-color: blue;
-        width:160px;
-        height:30px;
-        color:white;
-        margin-top: 20px;;
-      }
-      .row input{
-        border-radius: 50px;
-        width:350px;
-        height:30px;
-        
-    }
-      </style>
+    <title>Live Location</title>
 
+    <link rel="stylesheet" type="text/css" href="/SlRail/public/css/Passenger/styles_location.css">
+    <link rel="stylesheet" type="text/css" href="/SlRail/public/css/sidebar.css">
 </head>
 <body>
 
- <?php include('public/includes/header.php'); ?>
+    <?php include('public/includes/header.php'); ?>
 
-  <?php include('sm_sidebar.php'); ?>
+    <?php include('sm_sidebar.php'); ?>
 
-
-        <div class="container" style="text-align: center; margin-left:250px; background-color:white;">    
-            <h3>Track Location</h3>
+    <div class="row">
+        <div class="column">
+            <h3>View Live Location</h3>
+            <img src="/SlRail/public/assets/map.jpg" alt="Map" class="map-image">
         </div>
-            <div class="container" style="text-align: center; margin-left:300px;">
-                <img src="/SlRail/public/assets/map.jpg" style="width: 1000px; height: auto; display: inline-block;">
-            </div>
+    </div>
+    <div class="container">
+        <h1>Select Train</h1>
+            <form action="/SlRail/trainlocation/sm_view" method="post">
+                <select id="train_number" name="train_number" required>
+                    <?php foreach ($trains as $train): ?>
+                        <option value="<?= $train['train_number'] ?>">
+                            <?= $train['train_number'] ?> - <?= $train['departure_station'] ?> to <?= $train['destination_station'] ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+                <button type="submit" class="button-primary">View Location</button>
+            </form>
         </div>
-        <div class="container" style="margin-left: 400px; margin-top: 80px; ">
-            <div class="row">
-                <form action="/SlRail/trainlocation/sm_view" method="post">
-                  <label for="uname">Train No:</label>
-                  <input type="text" id="train_no" name="train_number" placeholder="      Enter Train No">
-                  <button type="submit" class="btn1">Search</button>
-              </div>
-        </div>
-   
+    </div>
 
     <?php include('public/includes/footer.php'); ?>
 
