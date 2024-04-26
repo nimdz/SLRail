84cd17bd87684110f5678bf4f6fd899a76858f55
Nimsara De Zoysa <105286867+nimdz@users.noreply.github.com>
2024-04-26 21:33:25 +0530

    Update TrainLocationController.php

--- app/controllers/TrainLocationController.php
+++ app/controllers/TrainLocationController.php
@@ -1,6 +1,8 @@
 <?php
  
  require_once 'app/models/Employee/TrainLocationModel.php';
+ require_once 'app/models/Employee/TrainModel.php';
+
 
  class TrainLocationController {
     private $model;
@@ -9,10 +11,20 @@
         $this->model = new TrainLocationModel();
     }
 
-    // Controller method to share train location
 
     public function load(){
-          
+
+        session_start();
+
+        $trainModel=new TrainModel();
+    
+        $employee_id=isset($_GET["employee_id"]) ? $_GET["employee_id"] : null;
+    
+        // Debug: Output the employee ID
+        //echo "Employee ID: " . $employee_id . "<br>";
+    
+        $trains=$trainModel->getAssignedTrains($employee_id);
+
         include('app/views/TrainDriver/location_share.php');
     
     }
@@ -51,6 +63,13 @@ public function location_form(){
         include('app/views/Passenger/location_search.php');
    
 }
+public function sm_location_form(){
+
+    $locationModel=new TrainLocationModel();
+    $trains=$locationModel->getTrainData();
+    include('app/views/StationMaster/sm_track_location.php');
+
+}
 public function view(){
     
     session_start();
@@ -64,6 +83,24 @@ public function view(){
     if (!empty($trainLocation)) {
         // Display the train location details
         include('app/views/Passenger/live_location_view.php');
+
+      } else {
+        echo "Train location not found.";
+    }
+}
+public function sm_view(){
+    
+    session_start();
+
+    $train_number=$_POST['train_number'];
+
+    $trainlocationModel=new TrainLocationModel();
+
+    $trainLocation=$trainlocationModel->getLocation($train_number);
+
+    if (!empty($trainLocation)) {
+        // Display the train location details
+        include('app/views/StationMaster/sm_view_location.php');
 
       } else {
         echo "Train location not found.";
